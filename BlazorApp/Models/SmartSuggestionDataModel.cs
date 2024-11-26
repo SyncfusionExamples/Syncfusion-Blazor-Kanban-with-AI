3847aad49045f438a324015175c9de4f0f0ff42e
ArunKumar-SF3979 <arunkumar.ragu@syncfusion.com>
2024-11-26 16:18:35 +0530

    Added Samples

---
+++ BlazorApp/Models/SmartSuggestionDataModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorApp.Models
+{
+    public class SmartSuggestionDataModel
+    {
+        // The title of the task.
+        public string Title { get; set; }
+
+        // The current status of the task.
+        public string Status { get; set; }
+
+        // The color associated with the task for visual representation.
+        public string Color { get; set; }
+
+        // A detailed description of the task.
+        public string Description { get; set; }
+
+        // The story points for the task, representing its complexity or effort.
+        public int StoryPoints { get; set; }
+    }
+}
+
